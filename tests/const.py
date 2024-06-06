db58f0a34f2ffc13fbe7e7729a375531e104321b
Connor Gallopo <connor.gallopo@me.com>
2024-06-06 15:12:44 -0400

    initial commit

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for leslies_pool tests."""
+from custom_components.leslies_pool.const import (
+    CONF_PASSWORD,
+)
+from custom_components.leslies_pool.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
